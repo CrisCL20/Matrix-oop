8a9f7a456409538acf3201287272599a15c57a1e
= <=>
2023-01-31 12:47:24 -0300

    Now I have the knowledge to plot a system of ode's

--- edo.cpp
+++ edo.cpp
@@ -1,48 +1,67 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <cstdio>
+#include <cmath>
 #include <math.h>
 #include <stdio.h>
 #include <fstream>
 
-#define n 10 //n value
-#define y0 2 //initial condition
+
+#define n 100 //n value
+#define m 2 //Number of ode's 
+#define y10 0 //initial condition for f1
+#define y20 0 //initial cond for f2
 
 using namespace std;
 
-double f1(double,double);
-
+double f1(double,double,double);
+double f2(double,double,double);
 
 int main(void)
 {
 
-    int a = 1,b = 2; //interval
-    double w = y0; //initial condition
+    int a = 0,b = M_PI; //interval
+    double w_1 = y10, w_2 = y20,dt;
 
     //step 1
     double h = ( (double) b - (double) a) / n;
     double t = a;
-    int i = 1;
+
+    double k1[m+1], k2[m+1], k3[m+1], k4[m+1]; //m+1 so we can index from 1 to m
 
     ofstream fout ("vis/diffeq.csv");
 
     if(!fout) cerr << "Could not open file\n";
 
     fout << scientific;
-    fout.precision(8);
+    fout.precision(5);
 
-    fout << "t" << "," << "y" << endl; 
+    fout << "t" << "," << "y" << "," << "w" << endl;
+    fout << t << "," << w_1 << "," << w_2 << endl;
 
-    //step 2
-    
-    while(i<=n){
-        w = w + h * f1(w,t);
-        t = a + i * h;
+    for(int i = 1;i <= n;i++){
+        
+        k1[1] = h * f1(w_1,w_2,t);
+        k1[2] = h * f2(w_1,w_2,t);
 
-        fout << t << "," << w << endl;
+        k2[1] = h * f1(w_1 + (k1[1] / 2),w_2 + (k1[2] / 2),t + (h / 2) );
+        k2[2] = h * f2(w_1 + (k1[1] / 2),w_2 + (k1[2] / 2),t + (h / 2) );
 
-        i++;
+        k3[1] = h * f1(w_1 + (k2[1] / 2),w_2 + (k2[2] / 2),t + (h / 2) );
+        k3[2] = h * f2(w_1 + (k2[1] / 2),w_2 + (k2[2] / 2),t + (h / 2) );
 
+        k4[1] = h * f1(w_1 + k3[1],w_2 + k3[2],t + h );
+        k4[2] = h * f2(w_1 + k3[1],w_2 + k3[2],t + h );
+
+        w_1 = w_1 + (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1])/6;
+        w_2 = w_2 + (k1[2] + 2 * k2[2] + 2 * k3[2] + k4[2])/6;
+
+        t = a + i*h;
+
+        fout << t << "," << w_1 << "," << w_2 << endl;
     }
+    
 
 
     return 0;
@@ -50,6 +69,10 @@ int main(void)
 }
 
 
-double f1(double y,double t){
-    return (1+t)/(1+y);
+double f1(double y1,double y2,double t){
+    return 6 - 4 * y1 + 3 * y2;
+}
+
+double f2(double y1, double y2, double t){
+    return 3.6 - 2.4 * y1 + 1.6 * y2;
 }
--- vis/source.js
+++ vis/source.js
@@ -3,31 +3,52 @@ let df = 'diffeq.csv';
 
 d3.csv(df).then(function(loadedData){
 
-    let data = [];
+    let data1 = [];
+    let data2 = [];
     let labels = [];
 
     for(let i = 0;i<loadedData.length;i++){
 
-        let t = loadedData[i].t;
-        let y = loadedData[i].y;
-        
+        console.log(loadedData[i]);
+
+        let t =  loadedData[i].t;
+        let y1 = loadedData[i].y;
+        let y2 = loadedData[i].w;
+
         labels.push(t);
-        data.push(y);
+        data1.push(y1);
+        data2.push(y2);
 
     }
+
+    var ds1 = {
+        label: "I1",
+        data: data1,
+        fill: false,
+        pointRadius : 0,
+        pointHoverRadius : 0,
+        borderColor : 'rgb(234,82,49)',
+        tension : 0.4
+    }
+
+    var ds2 = {
+        label : "I2",
+        data : data2,
+        fill : false,
+        pointRadius : 0,
+        pointHoverRadius : 0,
+        borderColor : 'rgb(36,87,232)',
+        tension : 0.4
+    }
+
 
     let options = {
         type: 'line',
         data: {
             labels : labels,
-            datasets : [{
-                data : data,
-                fill : false,
-                pointRadius : 0,
-                pointHoverRadius : 0,
-                borderColor : 'rgb(234,82,49)'
-            }]
-        }
+            datasets : [ds1,ds2]
+        },
+    
     };
 
     let chart = new Chart(document.getElementById('canvas'),options);
